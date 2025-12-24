c415f45c279f4f7b82171d97c16689f8500d25ba
fariz <farizrahman30@gmail.com>
2025-12-25 03:38:28 +0700

    Add event-scope to project

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -14,6 +14,15 @@ export const SOCIAL_MEDIA_ICONS = {
 
 export const PROJECTS = [
   {
+    title: "EventScope — Master Your Event Tracking",
+    description:
+      "The ultimate playground to design, simulate, and validate your analytics events. Ensure data quality with our Schema Builder and Event Simulator.",
+    image: "/projects/event-scope.jpg",
+    tags: ["Next.js", "TypeScript", "Tailwind CSS", "Shadcn UI", "Zustand"],
+    github: "https://github.com/ayisrhmn/event-scope",
+    demo: "https://event-scope-labs.vercel.app",
+  },
+  {
     title: "IngreAI — Ingredients Intelligence",
     description:
       "AI-Powered Ingredient Scanner & Recipe Generator. Scan your ingredients and get smart recipe suggestions instantly.",
